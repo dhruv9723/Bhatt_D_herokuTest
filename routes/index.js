517977da77b562d03a2cc8d3afecc20b1b5fdd39
Bhatt <d_bhatt8@tss-hr420-dm25.fc.ca>
2019-03-14 17:38:28 -0400

    dynamic1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const data = require('../data');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' , message:'Express is so hard even when you figure everything out ohh'});
+});
+
+
+//dynamic user via router paramaeter
+router.get('/:user', function(req, res, next) {
+  console.log('user:', data[req.params.user]);
+  res.render('userpage', data[req.params.user]);
 });
 
 module.exports = router;
